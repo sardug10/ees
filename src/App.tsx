8cbd6127f8b6a85bd20f42710419ba8c88075d42
Piyush Duggal <duggalpiyush0@gmail.com>
2024-07-31 03:05:08 +0530

    mesc

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AboutUs from "./components/AboutUs";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Nav from "./components/Navbar";
 import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
 import WhyChooseUs from "./components/WhyChooseUs";
@@ -9,12 +10,13 @@ import WhyChooseUs from "./components/WhyChooseUs";
 function App() {
   return (
     <div className="max-w-[1440px] mx-auto">
+      <Nav />
       <Hero />
       <Services />
       <AboutUs />
       <Testimonials />
       <WhyChooseUs />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
